5483fa49d785ab7da769ca33c984a776c038dc90
Büşra Akan <78661692+akanbusra@users.noreply.github.com>
2021-02-15 21:28:35 +0300

    dijital saat
    Timer ile dijital saat uygulaması

---
+++ timer_ile_dijital_saat/timer_ile_dijital_saat/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace timer_ile_dijital_saat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblsaat = new System.Windows.Forms.Label();
+            this.lbldakika = new System.Windows.Forms.Label();
+            this.lblsaniye = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblsaat
+            // 
+            this.lblsaat.AutoSize = true;
+            this.lblsaat.Font = new System.Drawing.Font("Cambria", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsaat.Location = new System.Drawing.Point(118, 87);
+            this.lblsaat.Name = "lblsaat";
+            this.lblsaat.Size = new System.Drawing.Size(31, 34);
+            this.lblsaat.TabIndex = 0;
+            this.lblsaat.Text = "0";
+            // 
+            // lbldakika
+            // 
+            this.lbldakika.AutoSize = true;
+            this.lbldakika.Font = new System.Drawing.Font("Cambria", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbldakika.Location = new System.Drawing.Point(240, 87);
+            this.lbldakika.Name = "lbldakika";
+            this.lbldakika.Size = new System.Drawing.Size(31, 34);
+            this.lbldakika.TabIndex = 1;
+            this.lbldakika.Text = "0";
+            // 
+            // lblsaniye
+            // 
+            this.lblsaniye.AutoSize = true;
+            this.lblsaniye.Font = new System.Drawing.Font("Cambria", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsaniye.Location = new System.Drawing.Point(359, 87);
+            this.lblsaniye.Name = "lblsaniye";
+            this.lblsaniye.Size = new System.Drawing.Size(31, 34);
+            this.lblsaniye.TabIndex = 2;
+            this.lblsaniye.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(616, 289);
+            this.Controls.Add(this.lblsaniye);
+            this.Controls.Add(this.lbldakika);
+            this.Controls.Add(this.lblsaat);
+            this.Font = new System.Drawing.Font("Cambria", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblsaat;
+        private System.Windows.Forms.Label lbldakika;
+        private System.Windows.Forms.Label lblsaniye;
+    }
+}
+
---
+++ timer_ile_dijital_saat/timer_ile_dijital_saat/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace timer_ile_dijital_saat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int saniye = 0, dakika = 0, saat = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            lblsaniye.Text = saniye.ToString();
+
+            if (saniye==60)
+            {
+                dakika++;
+                lbldakika.Text = dakika.ToString();
+                saniye = 0;
+
+                if (dakika==60)
+                {
+                    saat++;
+                    lblsaat.Text = saat.ToString();
+                    dakika = 0;
+                }
+            }
+        }
+    }
+}
